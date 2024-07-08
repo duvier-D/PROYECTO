6ff5843916175dc1c3ffadcd95ec4156b90fa2fb
duvier-D <duvierduarte@cdrbolivar.edu.co>
2024-07-08 13:35:09 -0500

    agrege un hola

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import vista.Vista;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("hola");
+
         Vista vista= new Vista();
         vista.run();
     }
