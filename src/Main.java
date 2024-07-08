ab44350189edca171392a426cc9b15b8d2ce514a
duvier-D <duvierduarte@cdrbolivar.edu.co>
2024-07-08 13:51:01 -0500

    borre el hola

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import vista.Vista;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hola");
+
 
         Vista vista= new Vista();
         vista.run();
