f92b8fae712eda603854a09c6828cb21146682c0
charly <krlos7384@gmail.com>
2024-08-20 10:55:09 -0500

    revisado por charly sin modificacion

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import vista.supervista;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
         supervista vista= new supervista();
         vista.run();
+        // revisado por charly sin modificacion
     }
 }
