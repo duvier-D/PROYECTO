3985ba93e10a488a6a753906fde838a393b0ac23
duvier-D <duvierduarte@cdrbolivar.edu.co>
2024-08-20 10:57:52 -0500

    borre el comentario de charly

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         supervista vista= new supervista();
         vista.run();
-        // revisado por charly sin modificacion
+
     }
 }
